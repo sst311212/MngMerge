006625a8d92a72a1f8b70ab9a29dc87bcfc4ce0f
sst311212 <sst311212@gmail.com>
2016-05-08 14:35:17 +0800

    first version

---
+++ composer/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace composer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Base Image:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Diff Image:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(81, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(291, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(81, 50);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(291, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(378, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(378, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Browse...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 86);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Save Diff";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(465, 121);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Composer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ composer/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace composer
+{
+    public partial class Form1 : Form
+    {
+        private Form Preview;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Preview = new Form();
+            Preview.Text = "Preview";
+            Preview.FormClosed += Preview_FormClosed;
+        }
+
+        private void Preview_FormClosed(object sender, EventArgs e)
+        {
+            Preview.Dispose();
+            Preview = new Form();
+            Preview.Text = "Preview";
+            Preview.FormClosed += Preview_FormClosed;
+        }
+
+        private Bitmap CreateBitmap(string FileName)
+        {
+            Bitmap tmpBmp = Bitmap.FromFile(FileName) as Bitmap;
+            Bitmap srcBmp = new Bitmap(tmpBmp);
+            tmpBmp.Dispose();
+            return srcBmp;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "圖片格式|*.bmp;*.png";
+            dialog.CheckFileExists = true;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = dialog.FileName;
+                Bitmap srcBmp = CreateBitmap(textBox1.Text);
+                Preview.Size = srcBmp.Size;
+                Preview.BackgroundImage = srcBmp;
+                Preview.Show();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "圖片格式|*.bmp;*.png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = dialog.FileName;
+                Bitmap srcBmp = CreateBitmap(textBox1.Text);
+                Bitmap dstBmp = Bitmap.FromFile(textBox2.Text) as Bitmap;
+                BitmapData srcData = srcBmp.LockBits(new Rectangle(0, 0, srcBmp.Width, srcBmp.Height), ImageLockMode.ReadWrite, srcBmp.PixelFormat);
+                BitmapData dstData = dstBmp.LockBits(new Rectangle(0, 0, dstBmp.Width, dstBmp.Height), ImageLockMode.ReadOnly, dstBmp.PixelFormat);
+                byte[] srcArray = new byte[srcData.Stride * srcData.Height];
+                byte[] dstArray = new byte[dstData.Stride * dstData.Height];
+                System.Runtime.InteropServices.Marshal.Copy(srcData.Scan0, srcArray, 0, srcArray.Length);
+                System.Runtime.InteropServices.Marshal.Copy(dstData.Scan0, dstArray, 0, dstArray.Length);
+                for (int i = 0; i < srcData.Height; i++)
+                    for (int j = 0; j < srcData.Stride; j++)
+                        srcArray[i * srcData.Stride + j] += dstArray[i * srcData.Stride + j];
+                System.Runtime.InteropServices.Marshal.Copy(srcArray, 0, srcData.Scan0, srcArray.Length);
+                srcBmp.UnlockBits(srcData);
+                dstBmp.UnlockBits(dstData);
+                dstBmp.Dispose();
+                Preview.BackgroundImage = srcBmp;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Preview.BackgroundImage.Save(textBox2.Text, ImageFormat.Png);
+        }
+    }
+}
